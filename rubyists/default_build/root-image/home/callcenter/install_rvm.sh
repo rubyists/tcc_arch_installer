5616dc608f0ace6b1f8c28b59c2005d1daf74395
Deryl R. Doucette <me@deryldoucette.com>
2012-02-05 14:44:17 -0600

    Added code to pull fs_specs repo, and create rvm gemset for fs_specs

--- rubyists/default_build/root-image/home/callcenter/install_rvm.sh
+++ rubyists/default_build/root-image/home/callcenter/install_rvm.sh
@@ -19,3 +19,15 @@ chmod 755 $HOME/.rvm/bin/rvm
 
 # Set up the bash shell's environment
 echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm" # Load RVM function' >> $HOME/.bash_profile
+
+# Now get into RVM set up the gemset for fs_specs
+source $HOME/.rvm/scripts/rvm
+
+# Get fs_specs
+git clone https://github.com/rubyists/fs_specs.git
+
+# Pre-Trust the project .rvmrc then make sure right Ruby and gemset is created
+rvm rvmrc trust $HOME/fs_specs/.rvmrc
+cd $HOME/fs_specs/
+rvm rvmrc load .
+
